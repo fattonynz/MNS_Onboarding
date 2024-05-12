507531c2e018a817f468b01356d512d453b07f64
vid-d <sachinviduranga@icloud.com>
2024-05-13 09:03:48 +1200

    test

--- MNS_Deploy/playbooks/library/fmgr_config_device_interface.py
+++ MNS_Deploy/playbooks/library/fmgr_config_device_interface.py
@@ -177,7 +177,8 @@ def main():
      module_arg_spec = {
         'device': {'required': True, 'type': 'str'},
         'interface': {'required': False, 'type': 'str'},
-        'allowaccess': {'required': False, 'type': 'str'}
+        'allowaccess': {'required': False, 'type': 'str'},
+        "state": {'required': False, 'type': 'str'}
      }
      module_option_spec = get_module_arg_spec('partial crud')
      module_arg_spec.update(module_option_spec)
